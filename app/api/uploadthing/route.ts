a77449533b485285849bfb4ed56506fcc4994a0a
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2024-11-28 17:49:15 +0530

    remove maxDuration

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,4 +1,3 @@
-export const maxDuration = 5; 
 import { createRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,4 +1,3 @@
-export const maxDuration = 5; 
 /* eslint-disable camelcase */
 // Resource: https://clerk.com/docs/users/sync-data-to-your-backend
 // Above article shows why we need webhooks i.e., to sync data to our backend
