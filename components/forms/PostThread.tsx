2459a318fabdf6e84918228afb560ba6ecdfd2fc
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2024-12-04 16:22:50 +0530

    complete organization feature

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -17,10 +17,12 @@ import * as z from "zod";
 import { usePathname, useRouter } from "next/navigation";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 
 export default function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -34,7 +36,7 @@ export default function PostThread({ userId }: { userId: string }) {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -2,6 +2,7 @@ import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
 import { currentUser } from "@clerk/nextjs/server";
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 
 interface Props {
   currentUserId: string;
@@ -33,13 +34,19 @@ interface Thread {
 }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-  let result = await fetchUserPosts(accountId);
+
+  let result: any;
+  if(accountType === "Community"){
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId)
+  }
 
   if (!result) redirect("/");
 
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {result[0].threads.map((thread: Thread) => (
+      {result.threads.map((thread: Thread) => (
         <ThreadCard
           key={thread._id}
           id={thread._id}
@@ -49,9 +56,9 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
           author={
             accountType === "User"
               ? {
-                  name: result[0].name,
-                  image: result[0].image,
-                  id: result[0].id,
+                  name: result.name,
+                  image: result.image,
+                  id: result.id,
                 }
               : {
                   name: thread.author.name,
@@ -59,7 +66,11 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
                   id: thread.author.id,
                 }
           }
-          community={thread.community}
+          community={
+            accountType === "Community"
+              ? { name: result.name, id: result.id, image: result.image }
+              : thread.community
+          }
           createdAt={thread.createdAt}
           comments={thread.comments}
         />
